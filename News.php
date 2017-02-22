c22b7a70db7adb3136b94ac447a56fdb430a1e44
Pro <pro@prosl.local>
2017-02-22 21:12:29 +0300

    финал ничего не понял

--- News.php
+++ News.php
@@ -5,25 +5,6 @@ class News extends Publication
 {
     public $important = false;
 
-    protected $propList = [
-        'id' => [
-            'type' => 'integer',
-            'required' => true
-        ],
-        'name' => [
-            'type' => 'string',
-            'required' => true
-        ],
-        'surname' => [
-            'type' => 'string',
-            'required' => false
-        ],
-        'important' => [
-            'type' => 'boolean',
-            'required' => 'false'
-        ]
-    ];
-
     public function draw() 
     {
         if ($this->important) 
--- Publication.php
+++ Publication.php
@@ -4,29 +4,7 @@ require_once ('ActiveRecord.php');
 
 abstract class Publication extends ActiveRecord
 {
-    public $title;
-    public $author;
-    public $publicationDate;
-
-    protected $propList = [
-        'id' => [
-            'type' => 'integer',
-            'required' => true
-        ],
-        'name' => [
-            'type' => 'string',
-            'required' => true
-        ],
-        'surname' => [
-            'type' => 'string',
-            'required' => false
-        ]
-    ];
-
-    function __construct()
-    {
-        $this->type = 'publication';
-    }
 
     abstract public function draw();
+
 }
